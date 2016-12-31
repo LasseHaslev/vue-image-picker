ad7a914c794ecc94fab0c1ca6efbb1f08888f542
Lasse S. Haslev <lasse@haslev.no>
2016-12-31 09:21:18 +0100

    Added images route

--- src/tutorials/server/server.js
+++ src/tutorials/server/server.js
@@ -6,13 +6,31 @@ var port = process.env.PORT || 1337;
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
 
+var fs = require( 'fs' );
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+app.use('/uploads', express.static('uploads'))
+var buildImagesPath = function( filename ) {
+    return 'http://localhost:' + port + '/uploads/' + filename;
+}
 // routes will go here
+app.get( '/api/images', function(req, res, next) {
+    var images = [];
+    var reg = /\.(?:jpg|gif|png)$/
+    fs.readdir( 'uploads/', function( err, files ) {
+        files.forEach( function( file ) {
+            if (file.match( reg )) {
+                images.push( buildImagesPath( file ) );
+            }
+        } )
+        res.send( images );
+    } );
+} );
 app.post( '/api/images', upload.single( 'image' ), function(req, res, next) {
     console.log(req.image);
     res.send( req.files );
