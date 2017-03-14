cade8d3ca2459e6d6c26d6dc8fcb91eb96e908f1
Lasse S. Haslev <lasse@haslev.no>
2017-03-14 08:27:48 +0100

    Created offline mode

---
+++ init.sh
@@ -0,0 +1,7 @@
+yarn
+mkdir offline
+mkdir offline/css
+mkdir offline/fonts
+wget https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.2/css/bulma.css -O 'offline/css/bulma.css'
+wget https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css -O 'offline/css/font-awesome.min.css'
+wget https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0 -O 'offline/fonts/fontawesome-webfont.woff'
