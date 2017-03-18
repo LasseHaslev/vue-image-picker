04456cf095e646431ef1c746e5a086778d82004c
Lasse S. Haslev <lasse@haslev.no>
2017-03-18 14:14:02 +0100

    Added props to index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ImagePicker from './components/ImagePicker';
 import ImagePickerItem from './components/ImagePickerItem';
 
+import Props from './components/mixins/Props';
+import Dropzone from './components/mixins/Dropzone';
+
 export default ImagePicker;
 export {
     ImagePicker,
     ImagePickerItem,
+    Props,
+    Dropzone,
 }
